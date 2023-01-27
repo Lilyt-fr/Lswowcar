705fc804d496ec91b01d02c901993630fe27c6f0
Lilyt <89300392+Lilyt-fr@users.noreply.github.com>
2023-01-27 19:20:50 +0100

    Add files via upload

---
+++ Lshowcar/config.lua
@@ -0,0 +1,17 @@
+ShowRange = 2000000
+
+DoorLock = true
+
+carInvincible = true 
+
+Cars = {
+-- endroit
+    {
+        pos = vec(-791.52, -217.84, 37.41),
+        heading = 192.56,
+        model = 'lp700r',
+        spin = true,
+        text = "LP700r \n ~y~10'000'000 $",
+        plate = "DEALER"
+    },
+}
